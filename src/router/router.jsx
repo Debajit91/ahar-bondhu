14d723e3b441a856bdcb61ccedd367737bfbbaf5
Debajit91 <debajitroy544@gmail.com>
2025-07-21 17:54:32 +0600

    home layout implemented

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,7 @@ import {
 } from "react-router";
 import HomeLayout from "../Layouts/HomeLayout";
 import Home from "../Pages/Home";
+import AvailableFoods from "../Pages/AvailableFoods";
 
 
 export const router = createBrowserRouter([
@@ -13,7 +14,14 @@ export const router = createBrowserRouter([
       {
         path: "/",
         Component: Home
+      },
+      {
+        path: "available-foods",
+        Component: AvailableFoods
       }
     ]
   },
+  {
+    path: "/"
+  }
 ]);
